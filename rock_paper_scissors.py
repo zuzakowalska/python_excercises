4143049a21f1e9cb0c7070e5b1a9513c0dd2e7c9
Zuzanna Kowalska <zuzanna.kowalska@brandnova.com>
2018-07-26 17:01:47 +0200

    damn you while loop

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,31 +1,42 @@
 import random
 
 moves = {1: "rock", 2: "scissors", 3: "paper"}
+win = 0
 
 
-def game_options(options):
-    print("Choose your weapon: ")
-    for option in options:
-        print(str(option) + ' ' + options[option])
-    u = int(input("Enter number: "))
-    if u not in list(options.keys()):
-        print("This choice is not valid")
-        game_options(options)
-    else:
-        print("You chose: " + options[u])
-        game(options, u)
+def game_options(options, win):
+    while win > -3 and win < 3:
+        print("Choose your weapon: ")
+        for option in options:
+            print(str(option) + ' ' + options[option])
+        u = int(input("Enter number: "))
+        if u not in list(options.keys()):
+            print("This choice is not valid")
+            game_options(options, win)
+        else:
+            print("You chose: " + options[u])
+            game(options, u, win)
+    if win == -3:
+        print("You lost")
+    elif win == 3:
+        print("You won")
 
 
-def game(options, u):
-    cp = random.choice(list(options.keys()))
+def game(options, u, win):
+    # cp = random.choice(list(options.keys()))
+    cp = 1
     print("Computer chose: " + options[cp])
     game_check = u - cp
     if game_check in [-1, 2]:
-        print("You won")
+        win += 1
     elif game_check in [-2, 1]:
-        print(" You lost")
-    elif game_check == 0:
-        print("It's a tie")
+        win -= 1
+    game_options(options, win)
+
+# w w w 3
+# w w l 1
+# w l l -1
+# l l l -3
 
 
-game_options(moves)
+game_options(moves, win)
